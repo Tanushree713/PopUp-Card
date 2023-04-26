707ef9a9722f613070386c1ba93aa83b5cd40594
Tanushree713 <123797833+Tanushree713@users.noreply.github.com>
2023-04-26 16:14:10 +0530

    Eighteenth Commit

---
+++ server1.js
@@ -0,0 +1,9 @@
+function popUp(){
+    let main =  document.getElementById("main");
+     main.style.display = 'block';
+}
+
+function popDown(){
+let main =  document.getElementById("main");
+main.style.display = "none";
+}
